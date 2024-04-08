4f88d63a30f3d9e4d11f30952421a415a7600f14
Daniel <donybof1@gmail.com>
2024-04-09 00:37:55 +0300

    Pancakes

--- obj/Debug/net8.0/Pancakes.AssemblyInfo.cs
+++ obj/Debug/net8.0/Pancakes.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pancakes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d6ebb7f4711b8bafd2607e54bf8461d4090cbb6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pancakes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pancakes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
